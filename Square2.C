460d6b5a0b479dc68624bde5bd1b0c6145455869
Harsh Vaibhav <145591840+HARSH-20031972@users.noreply.github.com>
2025-01-22 09:02:25 +0530

    Add files via upload

---
+++ Square2.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    float Side,Perimeter;
+    printf("Enter the Side \n");
+    scanf("%f",&Side);
+    Perimeter=4*Side;
+    printf("Perimeter is %f\n" ,Perimeter);
+    return 0;
+}
