309ea7bae4567b0f28840e9473c54a27dd434742
Harsh Vaibhav <145591840+HARSH-20031972@users.noreply.github.com>
2025-01-22 09:01:52 +0530

    Add files via upload

---
+++ Square1.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    int Side , Area;
+    printf("Enter the Side:");
+    scanf("%d",&Side);
+    Area=Side*Side;
+    printf("Area is %d",Area);
+    return 0;
+}   
