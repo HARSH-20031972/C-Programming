4f762084eb65fb6b053beb94d0bf772d92ed4308
Harsh Vaibhav <145591840+HARSH-20031972@users.noreply.github.com>
2025-01-25 08:29:37 +0530

    Add files via upload

---
+++ Subtract.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter Number a:");
+    scanf("%d", &a);
+    printf("Enter Number b:");
+    scanf("%d", &b);
+    c=a-b;
+    printf("Difference is %d", c);
+    return 0;
+}
