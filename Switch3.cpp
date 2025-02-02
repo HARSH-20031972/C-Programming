0f473c8bc015a3950deca3171459add5b89f4607
Harsh Vaibhav <145591840+HARSH-20031972@users.noreply.github.com>
2025-02-02 22:20:50 +0530

    Add files via upload

---
+++ Switch3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int Position=1;
+    switch(Position) {
+        case 1:
+        cout << "Gold\n";
+        break;
+        case 2:
+        cout << "Silver\n";
+        break;
+        case 3:
+        cout << "Bronze\n";
+        break;
+    }
+    return 0;
+}
